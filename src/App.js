95f05609499a2fa5e1aca3bc41db47bc48d7f969
KseniiaOldfield <kseniiaoldfield@gmail.com>
2023-04-15 22:52:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
 
 function App() {
+/* ПЕРВЫЙ СПОСОБ
+//////////////////////////////////////
+
+ /* const [advice, setAdvice] = useState("");
+
+  useEffect(() => {
+   const  getAdvice = async () => {
+    const responce = await fetch (`https://api.adviceslip.com/advice`)
+    const data = await responce.json()
+    console.log(data.slip.advice);
+    setAdvice(data.slip.advice)
+  }
+  getAdvice()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h2>{advice}</h2>
     </div>
-  );
+  );*/
+
+///////////////////////////////////////////
+/* 1 СПОСОБ ДЗ
+const [quoters, setQuoters] = useState("");
+
+useEffect(() => {
+  const getQuoters = async () => {
+    const responce = await fetch (`https://api.quotable.io/random`);
+    const data = await responce.json()
+    console.log(data.content)
+    setQuoters(data.content)
+  }
+  getQuoters()
+}, [])
+
+return (
+  <h2>{quoters}</h2>
+)*/
+
+/* 2 СПОСОБ ДЗ*/
+
+const [quoters, setQuoters] = useState("");
+  
+useEffect(() => {
+  getQuoters();
+}, [])
+
+const getQuoters = async () => {
+  const responce = await fetch (`https://api.quotable.io/random`);
+  const data = await responce.json();
+  console.log(data.content);
+  setQuoters(data.content)
+}
+return (
+  <h2>{quoters}</h2>
+);
 }
 
 export default App;
